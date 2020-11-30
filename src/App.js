a3134281e2b4df85574fd5b73e2939ccd8794ece
Alex <15ambm@queensu.ca>
2020-11-30 14:33:55 -0500

    - added ability to login - added Notification component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,15 @@
 import React, { useState, useEffect } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import loginService from './services/login'
+import Notification from './components/Notification'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
+  const [notification, setNotification] = useState({})
+  const [username, setUsername] = useState('')
+  const [password, setPassword] = useState('')
+  const [user, setUser] = useState(null)
 
   useEffect(() => {
     blogService.getAll().then(blogs =>
@@ -11,12 +17,64 @@ const App = () => {
     )  
   }, [])
 
+  const handleLogin = async (event) => {
+    event.preventDefault()
+    try {
+      const user = await loginService.login({username, password})
+      setUser(user)
+      setUsername('')
+      setPassword('')
+      setNotification({data:'Login Successful', color:'green'})
+      setTimeout(() => {
+        setNotification('')
+      }, 3000 )
+    } catch (e) {
+      setNotification({data:'Invalid Login Information', color:'red'})
+      setTimeout(() => {
+        setNotification('')
+      }, 3000 )
+    }
+  }
+
+  const loginForm = (
+    <form onSubmit={handleLogin}>  
+    <div>
+      <h4>Please log in</h4>
+      username
+      <input type='text'
+      value={username}
+      name='Username'
+      onChange={({target}) => setUsername(target.value)}/>
+    </div>
+    <div>
+      password
+      <input type='password'
+      value={password}
+      name='Password'
+      onChange={({target}) => setPassword(target.value)}/>
+    </div>
+    <button type='submit'>Login</button>
+  </form>
+  )
+
   return (
     <div>
       <h2>blogs</h2>
-      {blogs.map(blog =>
-        <Blog key={blog.id} blog={blog} />
-      )}
+
+      <Notification data={notification.data} color={notification.color}/>
+      {user === null ?
+        loginForm :
+        <div>
+          <p>{user.username} is logged in</p>
+          {blogs.map(blog =>
+          <Blog key={blog.id} blog={blog} />
+          )}
+        </div>
+      }
+
+
+
+
     </div>
   )
 }
---
+++ src/components/Notification.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Notification = ({ data, color }) => (
+  <div style={{color}}>
+    {data} 
+  </div>
+)
+
+export default Notification
