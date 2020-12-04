031d794037d21e2f837930490cf3fb7aef62d225
Alex <15ambm@queensu.ca>
2020-12-04 11:17:43 -0500

    - 5.5 added button to expand/collapse new blog form - note: 5.1-5.4 were completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef } from 'react'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
 import loginService from './services/login'
 import Notification from './components/Notification'
 import LoginForm from './components/LoginForm'
 import NewBlogForm from './components/NewBlogForm'
+import Togglable from './components/Togglable'
 
 const App = () => {
   const [blogs, setBlogs] = useState([])
@@ -12,8 +13,9 @@ const App = () => {
   const [username, setUsername] = useState('')
   const [password, setPassword] = useState('')
   const [user, setUser] = useState(null)
-  const [newBlogData, setNewBlogData] = useState({title:'', author:'', url:''})
 
+  const blogFormRef = useRef()
+  
   useEffect(() => {
     blogService.getAll().then(blogs =>
       setBlogs( blogs )
@@ -62,11 +64,10 @@ const App = () => {
     }, 3000 )
   }
 
-  const handleNewBlog = async (event) => {
-    event.preventDefault()
+  const handleNewBlog = async (newBlogData) => {
     try {
+      blogFormRef.current.toggleVisibility()
       const response = await blogService.create(newBlogData)
-      console.log(response)
       setBlogs(blogs.concat(response))
       setNotification({data:'Added new Blog', color:'blue'})
       setTimeout(() => {
@@ -79,7 +80,7 @@ const App = () => {
 
   return (
     <div>
-      <h2>blogs</h2>
+      <h2>Blogs</h2>
 
       <Notification data={notification.data} color={notification.color}/>
       {user === null ?
@@ -91,7 +92,9 @@ const App = () => {
         /> :
         <div>
           <p>{user.username} is logged in <button onClick={handleLogout}>Logout</button> </p>
-          <NewBlogForm handleNewBlog={handleNewBlog} newBlogData={newBlogData} setNewBlogData={setNewBlogData} />
+          <Togglable buttonLabel="New Blog" ref={blogFormRef}>
+            <NewBlogForm createBlog={handleNewBlog} />
+          </Togglable>
           {blogs.map(blog =>
           <Blog key={blog.id} blog={blog} />
           )}
--- src/components/NewBlogForm.js
+++ src/components/NewBlogForm.js
@@ -1,33 +1,46 @@
-import React from 'react'
+import React, {useState} from 'react'
 
-const NewBlogForm = ({ handleNewBlog, newBlogData, setNewBlogData}) => 
-(
-    <form onSubmit={handleNewBlog}>  
-    <h4>Add a blog</h4>
-    <div>
-      title
-      <input type='text'
-      value={newBlogData.title}
-      name='title'
-      onChange={({target}) => setNewBlogData({...newBlogData, title:target.value})}/>
-    </div>
-    <div>
-      author
-      <input type='text'
-      value={newBlogData.author}
-      name='author'
-      onChange={({target}) => setNewBlogData({...newBlogData, author:target.value})}/>
-    </div>
-    <div>
-      url
-      <input type='text'
-      value={newBlogData.url}
-      name='url'
-      onChange={({target}) => setNewBlogData({...newBlogData, url:target.value})}/>
-    </div>
-    <button type='submit'>Submit</button>
-  </form>
-  )
+const NewBlogForm = ({ createBlog }) => {
+  
+  const [newBlogData, setNewBlogData] = useState({title:'', author:'', url:''})
 
+  const handleNewBlog = async (event) => {
+    event.preventDefault()
+    try {
+      await createBlog(newBlogData)
+      setNewBlogData({title:'', author:'', url:''})
+    } catch (e) {
+      console.log(e)
+    }
+  }
+
+  return (
+      <form onSubmit={handleNewBlog}>  
+      <h4>Add a blog</h4>
+      <div>
+        title
+        <input type='text'
+        value={newBlogData.title}
+        name='title'
+        onChange={({target}) => setNewBlogData({...newBlogData, title:target.value})}/>
+      </div>
+      <div>
+        author
+        <input type='text'
+        value={newBlogData.author}
+        name='author'
+        onChange={({target}) => setNewBlogData({...newBlogData, author:target.value})}/>
+      </div>
+      <div>
+        url
+        <input type='text'
+        value={newBlogData.url}
+        name='url'
+        onChange={({target}) => setNewBlogData({...newBlogData, url:target.value})}/>
+      </div>
+      <button type='submit'>Submit</button>
+    </form>
+    )
+}
 
 export default NewBlogForm
